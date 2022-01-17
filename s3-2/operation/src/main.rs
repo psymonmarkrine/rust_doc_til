8b7202267f6935bf24d852de80b2d524f37cdfc2
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-01-17 21:40:14 +0900

    Operation

---
+++ s3-2/operation/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sum = 5 + 10;
+    println!("{}", sum);
+
+    let difference = 95.5 - 4.3;
+    println!("{}", difference);
+
+    let product = 4 * 30;
+    println!("{}", product);
+
+    let quotient = 56.7 / 32.2;
+    println!("{}", quotient);
+
+    let remainder = 43 % 5;
+    println!("{}", remainder);
+}
