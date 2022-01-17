518f745344a715122ca17dd53276cc2aeccd4c48
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-01-17 21:58:53 +0900

    Tuple2

--- s3-2/tuple/src/main.rs
+++ s3-2/tuple/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (x, y, z) = tup;
-
-    println!("The value of y is: {}", y);
+    println!("tup.0 is: {}", tup.0);
+    println!("tup.1 is: {}", tup.1);
+    println!("tup.2 is: {}", tup.2);
 }
