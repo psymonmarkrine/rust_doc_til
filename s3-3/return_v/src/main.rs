506def01b2097c853a915264cc5152db5ab59e17
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-01-18 22:56:39 +0900

    Return2

--- s3-3/return_v/src/main.rs
+++ s3-3/return_v/src/main.rs
@@ -1,10 +1,9 @@
+fn five() -> i32 {
+    5
+}
+
 fn main() {
-    let x = 5;
+    let x = five();
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
-
-    println!("The value of y is: {}", y);
+    println!("The value of x is: {}", x);
 }
