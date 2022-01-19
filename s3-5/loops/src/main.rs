9e7f0cf36e6d844909e599f76d3f5ee7a1fac9b9
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-01-19 22:15:32 +0900

    For2

--- s3-5/loops/src/main.rs
+++ s3-5/loops/src/main.rs
@@ -13,9 +13,14 @@ fn main() {
     // 
     // println!("Launch!");
 
-    let a = [10, 20, 30, 40, 50];
+    // let a = [10, 20, 30, 40, 50];
+    // 
+    // for element in a.iter() {
+    //     println!("The value is: {}", element);
+    // }
 
-    for element in a.iter() {
-        println!("The value is: {}", element);
+    for number in (1..4).rev() {
+        println!("{}?", number);
     }
+    println!("Launch!")
 }
