94101fa6cc4efe67e366c12efde42ef68ad60d5b
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-01-24 21:14:40 +0900

    first word?

---
+++ s4-3/first_word/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut s = String::from("THE QUICK BROWN FOX");
+    let word = first_word(&s);
+
+    println!("{}, {}", s, word);
+
+    s.clear();
+
+    println!("{}, {}", s, word);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
