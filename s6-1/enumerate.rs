16a8d43ca0f23ebd81e293c0a54bc8d6a233226b
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-01-29 23:03:15 +0900

    enumrate

---
+++ s6-1/enumerate.rs
@@ -0,0 +1,50 @@
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+// 
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+// 
+// let home = IpAddr {
+//     kind: IpAddrKind::V4,
+//     address: String::from("127.0.0.1"),
+// };
+// 
+// let loopback = IpAddr {
+//     kind: IpAddrKind::V6,
+//     address: String::from("::1"),
+// };
+
+// ########################################
+
+
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+// 
+// let home = IpAddr::V4(127, 0, 0, 1);
+// 
+// let loopback = IpAddr::V6(String::from("::1"));
+
+// ########################################
+
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+// 
+// impl Message {
+//     fn call(&self) {
+//         // method body would be defined here
+//         // メソッド本体はここに定義される
+//     }
+// }
+// 
+// let m = Message::Write(String::from("hello"));
+// m.call();
