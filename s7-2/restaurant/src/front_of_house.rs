aa6cb8e8e6afc26cda6a7a48de3d60c34563209c
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-07 22:13:05 +0900

    separate module into other file

---
+++ s7-2/restaurant/src/front_of_house.rs
@@ -0,0 +1,10 @@
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+    fn seat_at_table() {}
+}
+
+pub mod serving {
+    pub fn take_order() {}
+    fn serve_order() {}
+    fn take_payment() {}
+}
--- s7-2/restaurant/src/lib.rs
+++ s7-2/restaurant/src/lib.rs
@@ -1,15 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 pub use self::front_of_house::{hosting as hst, serving::*};
 
