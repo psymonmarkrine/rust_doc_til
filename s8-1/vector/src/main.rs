a8c94f8fd9d54d8fb34b92526177ebbbcedab80f
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-08 23:24:38 +0900

    out index  with "get"

--- s8-1/vector/src/main.rs
+++ s8-1/vector/src/main.rs
@@ -6,8 +6,11 @@ fn main() {
     v.push(7);
     v.push(8);
 
-    // let third: &i32 = &v[100];
-    let third: Option<&i32> = v.get(100);
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
 
     println!("{:#?}", third);
+
+    // let does_not_exist = &v[100];
+    let _does_not_exist = v.get(100);
 }
