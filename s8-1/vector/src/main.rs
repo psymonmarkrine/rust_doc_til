ecf19b4a6bc68e6832e3f44fab91e05aad86f477
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-08 23:53:56 +0900

    borrowed error

--- s8-1/vector/src/main.rs
+++ s8-1/vector/src/main.rs
@@ -1,16 +1,20 @@
 fn main() {
     let mut v = vec![1, 2, 3];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    // v.push(5);
+    // v.push(6);
+    // v.push(7);
+    // v.push(8);
 
-    let third: &i32 = &v[2];
+    // let third: &i32 = &v[2];
     let third: Option<&i32> = v.get(2);
 
-    println!("{:#?}", third);
+    // println!("{:#?}", third);
 
-    // let does_not_exist = &v[100];
+    // let _does_not_exist = &v[100];
     let _does_not_exist = v.get(100);
+
+    v.push(9);
+
+    println!("{:#?}", third);
 }
