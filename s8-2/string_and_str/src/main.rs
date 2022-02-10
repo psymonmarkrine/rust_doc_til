10945bd749a7bfce197619e2988f2979f5e102fe
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-10 20:49:04 +0900

    add String to String

--- s8-2/string_and_str/src/main.rs
+++ s8-2/string_and_str/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let s1 = String::from("foo");
-    let s2 = "bar";
-    let s3 = s1 + s2;
+    let s2 = String::from("bar");
+    let s3 = s1 + &s2;
 
     println!("{}", s2);
     println!("{}", s3);
