ac90cc1df411370a0de5880c15e37d8449a4f318
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-10 20:52:37 +0900

    "format!" macro

--- s8-2/string_and_str/src/main.rs
+++ s8-2/string_and_str/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     let s2 = String::from("bar");
     let s3 = s1 + &s2;
 
+    let s = format!("{}-{}", s2, s3);
+
     println!("{}", s2);
     println!("{}", s3);
+    println!("{}", s);
 }
