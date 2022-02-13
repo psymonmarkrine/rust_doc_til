c4958421e7c41f4be22903c6b57278cda43cffa2
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-13 18:27:49 +0900

    HashMap and ownership

--- s8-3/hashmap/src/main.rs
+++ s8-3/hashmap/src/main.rs
@@ -7,8 +7,19 @@ fn main() {
         ];
     let initial_scores = vec![10, 50];
     
-    let scores: HashMap<_, _> = teams.iter().
+    let mut scores: HashMap<_, _> = teams.iter().
         zip(initial_scores.iter()).collect();
 
+    let team_name = String::from("Green");
+    let team_score = 20;
+
+    // let mut scores = HashMap::new();
+    // scores.insert(team_name, team_score);
+
+    scores.insert(&team_name, &team_score);
+
     println!("{:#?}", scores);
+
+    println!("{}", team_name);
+    println!("{}", team_score);
 }
