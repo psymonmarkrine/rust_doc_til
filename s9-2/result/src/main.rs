cab67b55ef9b57af148638784d96f3fccff3ed7f
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-15 21:08:56 +0900

    Result "expect"

--- s9-2/result/src/main.rs
+++ s9-2/result/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
 
     // let f = match f {
     //     Ok(file) => file,
