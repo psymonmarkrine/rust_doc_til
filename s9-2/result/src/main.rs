5d1323291349be29500fec712c97047c4f2d36c2
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-18 21:48:53 +0900

    "?" operation2

--- s9-2/result/src/main.rs
+++ s9-2/result/src/main.rs
@@ -4,16 +4,8 @@ use std::io;
 use std::io::Read;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("hello.txt")?;
-
-    // let mut f = match f {
-    //     Ok(file) => file,
-    //     Err(e) => return Err(e),
-    // };
-
     let mut s = String::new();
-
-    f.read_to_string(&mut s)?;
+    File::open("hello.txt")?.read_to_string(&mut s)?;
     Ok(s)
 }
 
