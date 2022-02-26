863b519e5ec6b957cf4956f0c4932a4620c659c2
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-26 15:26:29 +0900

    method with generics

--- s10-1/struct_with_generic/src/main.rs
+++ s10-1/struct_with_generic/src/main.rs
@@ -4,8 +4,22 @@ struct Point<T, U> {
     y: U,
 }
 
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
     let int_and_float = Point { x: 2, y: 3.0 };
+
+    println!("integer.x = {}", integer.x());
+    println!("float.x = {}", float.x());
+    println!("int_and_float.x = {}", int_and_float.x());
 }
