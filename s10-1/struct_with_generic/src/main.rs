2a8be181e54ef6a88436fe2fd3b2dfb6ec410961
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-26 15:07:00 +0900

    struct with 2 generics

--- s10-1/struct_with_generic/src/main.rs
+++ s10-1/struct_with_generic/src/main.rs
@@ -1,10 +1,11 @@
 // https://docs.julialang.org/en/v1/manual/types/#man-parametric-composite-types
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
+    let int_and_float = Point { x: 2, y: 3.0 };
 }
