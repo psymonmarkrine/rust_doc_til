c9be355fb7aec0c048c01829ebc7d75d9c67ff95
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-27 16:15:56 +0900

    call trait's method 2

--- s10-2/trait_lib/src/lib.rs
+++ s10-2/trait_lib/src/lib.rs
@@ -1,6 +1,8 @@
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -12,6 +14,10 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+
     // fn summarize(&self) -> String {
     //     format!("{}, by {} ({})", self.headline, self.author, self.location)
     // }
@@ -25,7 +31,11 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
+
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
 }
