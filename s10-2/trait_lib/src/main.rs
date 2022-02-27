6dad1464da0896fef6432a8f02a2e4b4ba2c9ad3
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-02-27 16:07:24 +0900

    call trait's default method

--- s10-2/trait_lib/src/main.rs
+++ s10-2/trait_lib/src/main.rs
@@ -1,4 +1,4 @@
-use trait_lib::{Tweet, Summary};
+use trait_lib::{Tweet, NewsArticle, Summary};
 
 fn main() {
     let tweet = Tweet {
@@ -11,4 +11,16 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
