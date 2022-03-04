a29e102fcef28a827de722b2d8d7d800a2b931e7
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-03-04 21:58:21 +0900

    select by implemented trait

--- s10-2/trait_lib/src/lib.rs
+++ s10-2/trait_lib/src/lib.rs
@@ -74,3 +74,26 @@ fn some_function<T, U>(t: &T, u: &U) -> i32
 {}
 
 // */
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
