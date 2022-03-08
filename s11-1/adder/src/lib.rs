582d9dfc7e5d61dc83b29b6049b01942f006ebd8
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-03-08 20:44:19 +0900

    test functions

--- s11-1/adder/src/lib.rs
+++ s11-1/adder/src/lib.rs
@@ -1,8 +1,14 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        //このテストを失敗させる
+        panic!("Make this test fail");
     }
 }
