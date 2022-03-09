16f1f9d9e1dd7456b904b9069b286b5d84538b0d
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-03-09 21:58:16 +0900

    adder test with "assert_eq!"

--- s11-1/adder/src/lib.rs
+++ s11-1/adder/src/lib.rs
@@ -1,5 +1,16 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+/*
     #[test]
     fn exploration() {
         let result = 2 + 2;
@@ -11,4 +22,5 @@ mod tests {
         //このテストを失敗させる
         panic!("Make this test fail");
     }
+// */
 }
