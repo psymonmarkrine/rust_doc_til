f3319d5fe5caa5e3a56df6f6d8bdae0b85ceeecf
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-03-13 17:16:34 +0900

    store arguments in variables

--- s12-1/minigrep/src/main.rs
+++ s12-1/minigrep/src/main.rs
@@ -1,6 +1,11 @@
-use std::env:args;
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
