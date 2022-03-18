4cf00bf93f2bf9a269fcf45c2e0b4920ac16d67c
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-03-18 19:52:51 +0900

    add failing test

--- s12-1/minigrep/src/lib.rs
+++ s12-1/minigrep/src/lib.rs
@@ -30,3 +30,26 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
