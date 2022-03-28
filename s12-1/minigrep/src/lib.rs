f112b855bf66954b024f16582563ced48113d951
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-03-28 20:46:38 +0900

    change seach() to using iterator adopter method

--- s12-1/minigrep/src/lib.rs
+++ s12-1/minigrep/src/lib.rs
@@ -49,15 +49,9 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() { // do something with line
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
