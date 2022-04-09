46b4c6741fc59c247de121b989db942d5f9e48b0
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-04-09 20:16:06 +0900

    reference cycles?

--- s15-1/cons_list/src/main.rs
+++ s15-1/cons_list/src/main.rs
@@ -1,29 +1,61 @@
 use std::rc::Rc;
+use std::cell::RefCell;
 use List::{Cons, Nil};
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match *self {
+            Cons(_, ref item) => Some(item),
+            Nil => None,
+        }
+    }
+}
 
 fn main() {
-    let _list = Cons(1, 
-        Rc::new(Cons(2,
-            Rc::new(Cons(3,
-                Rc::new(Nil))))));
+    // let _list = Cons(1, 
+    //     Rc::new(Cons(2,
+    //         Rc::new(Cons(3,
+    //             Rc::new(Nil))))));
 
-    let a = Rc::new(Cons(5,
-                Rc::new(Cons(10,
-                    Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
+    // let a = Rc::new(Cons(5,
+    //             Rc::new(Cons(10,
+    //                 Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
 
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
 
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
     }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
