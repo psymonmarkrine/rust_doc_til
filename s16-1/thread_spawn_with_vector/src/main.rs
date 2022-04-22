262d89fb16ef1becf084bbeea7380e899addaf75
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-04-22 21:11:44 +0900

    "move" ownership to thread

--- s16-1/thread_spawn_with_vector/src/main.rs
+++ s16-1/thread_spawn_with_vector/src/main.rs
@@ -3,7 +3,7 @@ use std::thread;
 fn main() {
     let v = vec![1, 2, 3];
 
-    let handle = thread::spawn(|| {
+    let handle = thread::spawn(move || { 
         println!("Here's a vector: {:?}", v);
     });
 
