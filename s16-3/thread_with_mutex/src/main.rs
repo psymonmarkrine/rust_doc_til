3f1f14e9ca9400e30208e23617708053021779a3
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-04-28 21:32:16 +0900

    change mutex in Rc between thread

--- s16-3/thread_with_mutex/src/main.rs
+++ s16-3/thread_with_mutex/src/main.rs
@@ -1,11 +1,13 @@
 use std::sync::Mutex;
 use std::thread;
+use std::rc::Rc;
 
 fn main() {
-    let counter = Mutex::new(0);
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Rc::clone(&counter);
         let hundle = thread::spawn(move|| {
             let mut num = counter.lock().unwrap();
             *num += 1;
