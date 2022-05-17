a31c2471e7458f0f9ae1c8f81f435c9573ab8102
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-05-17 22:23:57 +0900

    add user's SelectBox

---
+++ s17-2/gui/src/user_lib.rs
@@ -0,0 +1,13 @@
+use gui::Draw;
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("process to draw a SelectBox");
+    }
+}
