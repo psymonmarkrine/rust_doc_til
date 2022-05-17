80452ee9a08cb9a9cbf4cb22d08918ca1486c7df
psymonmarkrine <47858465+psymonmarkrine@users.noreply.github.com>
2022-05-17 22:28:12 +0900

    fn main()

---
+++ s17-2/gui/src/main.rs
@@ -0,0 +1,27 @@
+use gui::{Button, Screen};
+
+mod user_lib;
+use user_lib::SelectBox;
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
